2e7f060b05849e327a89e431d75fc8b8f7fe0248
JC-29068 <ruttalajayachandra@gmail.com>
2026-03-24 10:16:29 +0530

    Update BookMyStay.java

--- App/src/BookMyStay.java
+++ App/src/BookMyStay.java
@@ -1,1 +1,6 @@
-
+public class UseCase1HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("Application: Hotel Booking System v1.0");
+    }
+}
